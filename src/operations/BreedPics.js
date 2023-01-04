9c185509b39086f7048074c11f2b6bebbd2822f5
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-05 00:41:02 +0400

    Clear linter errors

--- src/operations/BreedPics.js
+++ src/operations/BreedPics.js
@@ -1,13 +1,12 @@
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
   const xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false );
-  xmlHttp.send( null );
+  xmlHttp.open('GET', theUrl, false);
+  xmlHttp.send(null);
   return xmlHttp.responseText;
 }
 
 const Images = (breedName) => {
-	const json = httpGet(`https://dog.ceo/api/breed/${breedName}/images`);
+  const json = httpGet(`https://dog.ceo/api/breed/${breedName}/images`);
   const array = JSON.parse(json);
   const url = array.message;
   return url.length;
