d86dc7e511f07c9ba9a9c5da6c0f3f4d0020b338
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-07 05:10:44 +0400

    Clear linter errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,37 +5,39 @@ import { RiSettings5Fill } from 'react-icons/ri';
 import './NavBar.css';
 
 const NavBar = ({ page, routeTo }) => {
-  <ul className="nav-header">
-    <li>
-      {
-        routeTo === 0
-          ? (
-            <>
-              <FaChevronLeft />
-              <span className="arrow-back">
-                2015
-              </span>
-            </>
-          ) : (
-            <NavLink to="/" className="back-link">
-              <FaChevronLeft />
-              <span className="arrow-back">
-                2015
-              </span>
-            </NavLink>
-          )
-      }
-    </li>
-    <li className="nav-title">
-      {page}
-    </li>
-    <li className="icons">
-      <FaMicrophone />
-    </li>
-    <li className="icons-last">
-      <RiSettings5Fill />
-    </li>
-  </ul>;
+  return (
+    <ul className="nav-header">
+      <li>
+        {
+          routeTo === 0
+            ? (
+              <>
+                <FaChevronLeft />
+                <span className="arrow-back">
+                  2015
+                </span>
+              </>
+            ) : (
+              <NavLink to="/dog-breeds" className="back-link">
+                <FaChevronLeft />
+                <span className="arrow-back">
+                  2015
+                </span>
+              </NavLink>
+            )
+        }
+      </li>
+      <li className="nav-title">
+        {page}
+      </li>
+      <li className="icons">
+        <FaMicrophone />
+      </li>
+      <li className="icons-last">
+        <RiSettings5Fill />
+      </li>
+    </ul>
+  );
 };
 
 NavBar.propTypes = {
